25cceefd17c096f588bbdefdf1adbb41b64edc53
YuvinR <yuvin.agrithmics@gmail.com>
2022-09-29 17:13:06 +0530

    min

---
+++ SwivelProject/Assessment.CakeShop.EFCore/EfCoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Assessment.CakeShop.EFCore
+{
+    public class EfCoreDbContext : DbContext
+    {
+        public EfCoreDbContext(DbContextOptions<EfCoreDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Object> Data { get; set; }
+    }
+}
